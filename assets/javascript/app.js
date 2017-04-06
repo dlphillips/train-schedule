d92ab1b5602f04bc5d708ad4d799bfb1e93ce21c
Dan Phillips <dlphillips@gmail.com>
2017-04-06 14:32:55 -0400

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,12 +9,15 @@ $(document).ready(function() {
         storageBucket: "train-schedule-39c5a.appspot.com",
         messagingSenderId: "1052159302902"
     };
+
     firebase.initializeApp(config);
 
+    // initialize variables
     var database = firebase.database();
     var nextTrain = 0;
     var tMinutesTillTrain = 0;
 
+    // clears form fields
     function clearForm() {
         $("#tName").val("");
         $("#tDest").val("");
@@ -22,6 +25,7 @@ $(document).ready(function() {
         $("#tFreq").val("");
     }
 
+    // gets entries from form and adds to firebas db
     $("#tButton").on("click", function(event) {
         event.preventDefault();
 
@@ -41,39 +45,31 @@ $(document).ready(function() {
 
     });
 
-    function calcNextTrain(p1, p2) {
+        // given frequency and start time, this will return the time the next train will arrive as well as the minutes from arrival
+       function calcNextTrain(p1, p2) {
 
         var tFrequency = p1;
         var firstTime = p2;
 
         // First Time (pushed back 1 year to make sure it comes before current time)
         var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-        // console.log(firstTimeConverted);
 
-        // Current Time
         var currentTime = moment();
 
-        // Difference between the times
         var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
-        // Time apart (remainder)
         var tRemainder = diffTime % tFrequency;
-        // console.log(tRemainder);
 
-        // Minutes Until Train
         tMinutesTillTrain = tFrequency - tRemainder;
-        // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-        // Next Train
         nextTrain = moment().add(tMinutesTillTrain, "minutes");
         nextTrain = moment(nextTrain).format("hh:mm A");
 
         return [nextTrain, tMinutesTillTrain];
-
     };
 
+    // called when data is added to firebase. populates the table body with all the train data.
     database.ref().on("child_added", function(snapshot) {
-
         $("#trainInfo").append(`
             <tr>
                 <td>${snapshot.val().name}</td>
@@ -83,26 +79,51 @@ $(document).ready(function() {
                 <td>${calcNextTrain(snapshot.val().frequency,snapshot.val().starttime)[1]}</td>
                 <td><button type="button" id="${snapshot.key}" class="btn btn-default edit-btn"><span class="glyphicon glyphicon-pencil"</span></button></td>
                 <td><button type="button" id="${snapshot.key}" class="btn btn-default delete-btn"><span class="glyphicon glyphicon-trash"</span></button></td>
-                <td></td>
             </tr>
             `);
     });
 
+    // removes the train infor based on the row that was clicked
     function deleteTrain() {
         if (confirm("Are you sure you want to delete this train?")) {
-
-            alert("Record deleted");
-        } else {
-            alert("Nothing deleted");
+            database.ref().child($(this).attr('id')).remove();
+            $(this).closest('tr').remove();
         };
+    }
 
-        console.log($(this).attr('id'));
+    // retrieves data for train from firebase and populates the form. removes data from firebase. 
+    function editTrain() {
 
-        // tmp = $(this).attr('src');
-        // $(this).attr('src', $(this).attr('alt_src'));
-        // $(this).attr('alt_src', tmp);
-    } 
-    
+        var row_num = $(this).parent().parent().index();
+        var clickKey = $(this).attr('id');
+
+        var query = database.ref().orderByKey();
+        query.once("value")
+            .then(function(snapshot) {
+                var snapObj = snapshot.val();
+                snapshot.forEach(function(childSnapshot) {
+                    var key = childSnapshot.key;
+                    // console.log(key);
+                    var childData = childSnapshot.val();
+
+                    if (key === clickKey) {
+                        $("#tName").val(childData.name);
+                        $("#tDest").val(childData.destination);
+                        $("#tTime").val(childData.starttime);
+                        $("#tFreq").val(childData.frequency);
+                    }
+                });
+            });
+        database.ref().child($(this).attr('id')).remove();
+        $(this).closest('tr').remove();
+    }
+
+    // listeners for edit & delete buttons in table. 
+    $(document).on("click", ".edit-btn", editTrain);
     $(document).on("click", ".delete-btn", deleteTrain);
+
+    // play some good music for chris
+    var snd = new Audio("assets/sounds/fpb.mp3");
+    snd.play();
 
 });
